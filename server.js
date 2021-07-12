5dc5491f46d45540e6d100286ad9f75364fbf6a8
Aman7445 <ak858696@gmail.com>
2021-07-12 18:16:20 +0530

    calc distance

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const expressGraphQl = require('express-graphql').graphqlHTTP;
 const schema = require('./schema.js');
+const distance= require('./distance/distance.js');
 
 const app = express();
 
@@ -8,5 +9,11 @@ app.use('/graphql', expressGraphQl({
     schema: schema,
     graphiql: true
 }));
+app.get('/',(req ,res)=>{
+    const ans=distance.getDistance(28.565307,77.122413,28.45437,77.07268);
+    return(
+        res.json(ans)
+    );
+});
 
 app.listen(5000, () => console.log(`Server is running on port 5000`));
