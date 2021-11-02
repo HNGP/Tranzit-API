7a73a0a3eb69762b2c626575598e0938a7e1677c
Aman7445 <ak858696@gmail.com>
2021-11-02 17:13:24 +0530

    added cors

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const expressGraphQl = require("express-graphql").graphqlHTTP;
 const schema = require("./schema.js");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 
 app.use(
   "/graphql",
