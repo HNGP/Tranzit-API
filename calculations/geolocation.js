134056480c7ac23cf1ae4d631a2a22c386a44b61
ashwin-sekar <ashwinsekar14@gmail.com>
2022-01-06 23:44:36 +0530

    added igi t3

--- calculations/geolocation.js
+++ calculations/geolocation.js
@@ -1,13 +1,17 @@
 const { getDistance } = require("../utils/util");
-const data = require("../stations/delhi-data.json");
+const data = require("../scripts/data.json");
+
+for (let i = 0; i < Object.keys(data).length; i++) {
+  console.log(data[i + 1].latitude);
+}
 
 let findNearestStation = (user_latitude, user_longitude) => {
   let lat2, lon2, dist;
   let distanceList = [];
 
-  for (let i = 0; i < data.stations.length; i++) {
-    lat2 = data.stations[i].details.latitude;
-    lon2 = data.stations[i].details.longitude;
+  for (let i = 0; i < Object.keys(data).length; i++) {
+    lat2 = data[i + 1].latitude;
+    lon2 = data[i + 1].longitude;
     dist = getDistance(user_latitude, user_longitude, lat2, lon2);
     distanceList.push(dist);
   }
@@ -16,7 +20,7 @@ let findNearestStation = (user_latitude, user_longitude) => {
   i = distanceList.indexOf(leastDistance);
 
   const geoResult = {
-    nearestStation: data.stations[i].title,
+    nearestStation: data[i + 1].title,
     distance: leastDistance,
   };
 
